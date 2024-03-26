71a22db4888ec0a23e6f0f00339e1fa878a3a58b
samwel Okoth <samwelokoth8@gmail.com>
2024-03-26 13:25:34 +0300

    Clients configuration file (w/Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,10 @@
+# set up your client SSH configuration file so that you can connect to # a server without typing a password using puppet
+
 file { '/home/p3nt3st4r/.ssh/config':
   ensure  => file,
   mode    => '0600',
   content => "\
-Host your_server_ip
+Host 54.166.77.54
     IdentityFile ~/.ssh/school
     PreferredAuthentications publickey
     PasswordAuthentication no
